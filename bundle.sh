6cf4ecfff86759210e3cfc1ef4a033b7dabd3b3c
Hyungjun Lim <sculd3@gmail.com>
2021-08-14 23:57:08 -0400

    fix the bundle script

--- bundle.sh
+++ bundle.sh
@@ -6,4 +6,5 @@ cd package
 zip -r ../my-deployment-package.zip .
 cd ..
 zip -g my-deployment-package.zip lambda_function.py
+zip -g my-deployment-package.zip authorize.py
 aws s3 cp my-deployment-package.zip s3://market-signal-checkout-lambda/
